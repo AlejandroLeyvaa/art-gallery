1e26cff75dfcbfe903706887c7eb02e814ef3950
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2021-06-12 08:27:17 -0600

    No commit messages

--- public/utils/getData.js
+++ public/utils/getData.js
@@ -2,6 +2,7 @@ export async function getData(API) {
   try {
     const response = await fetch(API);
     const data = response.json();
+    console.log(data, 'Response');
     if(typeof data.data === 'string') {
       alert(data.data);
       return data.data;
